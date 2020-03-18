- Enable plotting and exploring the task/pheno latent spaces - Fix bug: pheno dict from english to german - Fix bug: if possible average task is taken per patient not the grand average - Add 50 factor for beta-VAE testing

scripts/Spatiotemporal_VAE/ConditionalModel.py
@@ -86,9 +86,9 @@ class ConditionalSpatioTemporalVAE(SpatioTemporalVAE):
         recon_motion, recon_pose_z_seq, concat_motion_z = self.decode(
             motion_z, labels
         )  # Convert (m, motion_latent_dim+label_dim) to (m, fea, seq)
-        pred_labels = self.class_net(concat_motion_z)  # Convert (m, motion_latent_dim+label_dim) to (m, n_classes)
+        pred_labels, task_latent = self.class_net(concat_motion_z)  # Convert (m, motion_latent_dim+label_dim) to (m, n_classes)
         return recon_motion, pred_labels, (pose_z_seq, recon_pose_z_seq, pose_mu, pose_logvar), (
-            motion_z, motion_mu, motion_logvar)
+            motion_z, motion_mu, motion_logvar), task_latent
 
     def encode(self, x, labels):
         # Concatenation of input for encoding
@@ -194,15 +194,13 @@ class PhenotypeNet(nn.Module):
 
         motion_z, tasks, tasks_mask, patient_ids, phenos, phenos_mask = inputs
         fingerprint, uni_phenos = self._transform_to_patient_task_means(motion_z, tasks, tasks_mask, patient_ids, phenos, phenos_mask)
-
         out = self.layer_in(fingerprint)
         out = self.layer2(out)
-        out = self.layer_out(out)
-        out = self.sigmoid_layer(out)
+        pheno_latent = self.layer_out(out)
+        out = self.sigmoid_layer(pheno_latent)
+        return out, uni_phenos, pheno_latent
 
-        return out, uni_phenos
     def _transform_to_patient_task_means(self, motion_z, tasks, tasks_mask, patient_ids, phenos, phenos_mask):
-
         # Masking
         true_mask = (tasks_mask == 1) & (np.isnan(patient_ids) == False) & (phenos_mask == 1)
 
@@ -244,7 +242,7 @@ class PhenotypeNet(nn.Module):
             for j in range(self.task_dim):
                 patient_task_mask = (tasks == j) & (patient_ids == patient_ids[p_id])
                 if np.sum(patient_task_mask) > 0:
-                    average_patient_task = torch.mean(numpy_bool_index_select(tensor_arr=sliced_z, mask=(tasks == j),
+                    average_patient_task = torch.mean(numpy_bool_index_select(tensor_arr=sliced_z, mask=((tasks == j) & (patient_ids == patient_ids[p_id])),
                                                                               device=self.device),
                                                       dim=0)
                 else:
@@ -302,10 +300,11 @@ class ConditionalPhenotypeSpatioTemporalVAE(ConditionalSpatioTemporalVAE):
         recon_motion, recon_pose_z_seq, concat_motion_z = self.decode(
             motion_z, labels
         )  # Convert (m, motion_latent_dim+label_dim) to (m, fea, seq)
-        pred_labels = self.class_net(concat_motion_z)  # Convert (m, motion_latent_dim+label_dim) to (m, n_classes)
+        pred_labels, task_latent = self.class_net(concat_motion_z)  # Convert (m, motion_latent_dim+label_dim) to (m, n_classes)
 
         # Identification net
-        pred_identify, labels_identify = self.phenotype_net(motion_z, tasks_np, tasks_mask_np, patient_ids_np, phenos_np, phenos_mask_np)
+        pred_identify, labels_identify, pheno_latent = self.phenotype_net(motion_z, tasks_np, tasks_mask_np, patient_ids_np, phenos_np, phenos_mask_np)
 
         return recon_motion, pred_labels, (pose_z_seq, recon_pose_z_seq, pose_mu, pose_logvar), (
-            motion_z, motion_mu, motion_logvar), (pred_identify, labels_identify)
+            motion_z, motion_mu, motion_logvar), (pred_identify, labels_identify, pheno_latent), task_latent
+

scripts/Spatiotemporal_VAE/Containers.py
@@ -96,7 +96,7 @@ class BaseContainer:
             self.model, self.optimizer, self.lr_scheduler = self._model_initialization()
         else:
             self.model, self.optimizer, self.lr_scheduler = self._load_model()
-        # self._save_model()  # Enabled only for renewing newly introduced hyper-parameters
+        #self._save_model()  # Enabled only for renewing newly introduced hyper-parameters
 
     def forward_evaluate(self, datagen_tuple):
         self.model.eval()
@@ -266,7 +266,7 @@ class BaseContainer:
     def loss_function(self, model_outputs, inputs_info):
         # Unfolding tuples
         x, nan_masks, tasks, tasks_mask = inputs_info
-        recon_motion, pred_labels, pose_info, motion_info = model_outputs
+        recon_motion, pred_labels, pose_info, motion_info, task_latent = model_outputs
         pose_z_seq, recon_pose_z_seq, pose_mu, pose_logvar = pose_info
         motion_z, motion_mu, motion_logvar = motion_info
 
@@ -598,21 +598,21 @@ class PhenoCondContainer(BaseContainer):
     def loss_function(self, model_outputs, inputs_info):
         # Unfolding tuples
         x, nan_masks, tasks, tasks_mask = inputs_info
-        recon_motion, pred_labels, pose_info, motion_info, phenos_info = model_outputs
+        recon_motion, pred_labels, pose_info, motion_info, phenos_info, task_latent = model_outputs
         pose_z_seq, recon_pose_z_seq, pose_mu, pose_logvar = pose_info
         motion_z, motion_mu, motion_logvar = motion_info
-        phenos_pred, phenos_labels_np = phenos_info
+        phenos_pred, phenos_labels_np, pheno_latent = phenos_info
 
 
         # Posenet kld
         posenet_kld_multiplier = self._get_interval_multiplier(self.posenet_kld)
         posenet_kld_loss_indicator = -0.5 * torch.mean(1 + pose_logvar - pose_mu.pow(2) - pose_logvar.exp())
-        posenet_kld_loss = posenet_kld_multiplier * posenet_kld_loss_indicator
+        posenet_kld_loss = 50 * posenet_kld_multiplier * posenet_kld_loss_indicator
 
         # Motionnet kld
         motionnet_kld_multiplier = self._get_interval_multiplier(self.motionnet_kld)
         motionnet_kld_loss_indicator = -0.5 * torch.mean(1 + motion_logvar - motion_mu.pow(2) - motion_logvar.exp())
-        motionnet_kld_loss = motionnet_kld_multiplier * motionnet_kld_loss_indicator
+        motionnet_kld_loss = 50 * motionnet_kld_multiplier * motionnet_kld_loss_indicator
 
         # Recon loss
         diff = x - recon_motion

scripts/Spatiotemporal_VAE/Model.py
@@ -223,9 +223,9 @@ class SpatioTemporalVAE(nn.Module):
     def forward(self, x):
         (pose_z_seq, pose_mu, pose_logvar), (motion_z, motion_mu, motion_logvar) = self.encode(x)
         recon_motion, recon_pose_z_seq = self.decode(motion_z)  # Convert (m, motion_latent_dim) to (m, fea, seq)
-        pred_labels = self.class_net(motion_z)  # Convert (m, motion_latent_dim) to (m, n_classes)
+        pred_labels, task_latent = self.class_net(motion_z)  # Convert (m, motion_latent_dim) to (m, n_classes)
         return recon_motion, pred_labels, (pose_z_seq, recon_pose_z_seq, pose_mu, pose_logvar), (
-        motion_z, motion_mu, motion_logvar)
+        motion_z, motion_mu, motion_logvar), task_latent
 
     def encode(self, x):
         out = self.transpose_flatten(x)  # Convert (m, fea, seq) to (m * seq, fea)
@@ -571,9 +571,9 @@ class TaskNet(nn.Module):
         self.sigmoid_layer = nn.Sigmoid()
 
     def forward(self, x):
-        out = self.encode(x)
-        out = self.sigmoid_layer(out)
-        return out
+        task_latent = self.encode(x)
+        out = self.sigmoid_layer(task_latent)
+        return out, task_latent
 
     def encode(self, x):
 

scripts/Spatiotemporal_VAE/analysis_scripts/thesis_save_model_outputs.py
@@ -56,7 +56,6 @@ class OutputSavers:
 
 
     def forward_batch(self):
-
         # Dummy iteration to dump all test data
         for _, test_data in self.data_gen.iterator():
             pass
@@ -79,14 +78,14 @@ class OutputSavers:
             print("forward passing {}".format(identifier))
             data_outputs = model_container.forward_evaluate(test_data)
             if identifier == "B+C+T+P":  # PhenotypeNet has extra columns to store in separate dataframe
-                recon, pred_task, _, motion_info, phenos_info = data_outputs
-                phenos_pred, phenos_labels_np = phenos_info
+                recon, pred_task, _, motion_info, phenos_info, task_latent = data_outputs
+                phenos_pred, phenos_labels_np, pheno_latent = phenos_info
             else:
-                recon, pred_task, _, motion_info = data_outputs
-                phenos_pred, phenos_labels_np = None, None
+                recon, pred_task, _, motion_info, task_latent = data_outputs
+                phenos_pred, phenos_labels_np, pheno_latent = None, None, None
 
             motion_z, _, _ = motion_info
-            data_to_record = (recon, motion_z, pred_task, phenos_pred, phenos_labels_np)
+            data_to_record = (recon, motion_z, pred_task, phenos_pred, phenos_labels_np, pheno_latent, task_latent)
 
             # Store data into self.df_dict aand self.df_pheno_dict in each loop, for creating an overall dataframe later
             # Umap transformation is also done below
@@ -97,17 +96,24 @@ class OutputSavers:
 
     def _record_data_by_identifier(self, identifier, data_to_record):
 
-        (recon, motion_z, pred_task, phenos_pred, phenos_labels_np) = data_to_record
-        recon, motion_z, pred_task = tensor2numpy(recon, motion_z, pred_task)
+        (recon, motion_z, pred_task, phenos_pred, phenos_labels_np, pheno_latent, task_latent) = data_to_record
+        recon, motion_z, pred_task, task_latent = tensor2numpy(recon, motion_z, pred_task, task_latent)
 
         if identifier == "B+C+T":
             self.df_dict["{}_pred_task".format(identifier)] = list(np.argmax(pred_task, axis=1))
+            self.df_dict["{}_task_latent".format(identifier)] = list(task_latent)
+            self.df_dict["{}_tl_umap".format(identifier)] = list(self._umap_transform(task_latent))
 
         elif identifier == "B+C+T+P":
-            phenos_pred = tensor2numpy(phenos_pred)[0]
+            phenos_pred, pheno_latent = tensor2numpy(phenos_pred, pheno_latent)
             self.df_dict["{}_pred_task".format(identifier)] = list(np.argmax(pred_task, axis=1))
+            self.df_dict["{}_task_latent".format(identifier)] = list(task_latent)
+            self.df_dict["{}_tl_umap".format(identifier)] = list(self._umap_transform(task_latent))
             self.df_pheno_dict["pheno_pred"] = list(np.argmax(phenos_pred, axis=1))
             self.df_pheno_dict["pheno_labels"] = list(phenos_labels_np)
+            self.df_pheno_dict["pheno_latent"] = list(pheno_latent)
+            #print('\n\n\n pheno latent: ', list(pheno_latent), '\n\n\n')
+            self.df_pheno_dict["pheno_umap"] = list(self._umap_transform(pheno_latent))
         self.df_dict["{}_recon".format(identifier)] = list(recon)
         self.df_dict["{}_z".format(identifier)] = list(motion_z)  # z = latent vector
         self.df_dict["{}_z_umap".format(identifier)] = list(self._umap_transform(motion_z))

scripts/common/generator.py
@@ -172,8 +172,9 @@ class GaitGeneratorFromDFforTemporalVAE(GaitGeneratorFromDF):
         # Construct df filtered out the nan
         self.df_nonan = self._construct_filtered_df()
 
-    def _split_train_test(self):
+        self.pheno_stats = []
 
+    def _split_train_test(self):
         labelled_mask = (self.df["task_masks"] == True)
         df_test = self.df[labelled_mask][0:8000].copy()
         train_index = list(self.df[labelled_mask][8000:].index) + list(self.df[labelled_mask == False].index)
@@ -184,8 +185,9 @@ class GaitGeneratorFromDFforTemporalVAE(GaitGeneratorFromDF):
         selected_df = df_shuffled.iloc[start:stop, :].copy()
 
         if self.gait_print:
-            selected_df = self._complete_gaitprint(selected_df)
-            selected_df_test = self._complete_gaitprint(self.df_test.sample(n=self.mt))
+            selected_df, num_uni_ids_pheno_train = self._complete_gaitprint(selected_df)
+            selected_df_test, num_uni_ids_pheno_test = self._complete_gaitprint(self.df_test.sample(n=self.mt))
+            #self.pheno_stats = self.pheno_stats + num_uni_ids_pheno_train
         else:
             selected_df_test = self.df_test.sample(n=self.mt)
 
@@ -290,6 +292,7 @@ class GaitGeneratorFromDFforTemporalVAE(GaitGeneratorFromDF):
         current_uni_ids = np.unique(df[id_nan_mask]["idpatients"])
 
         indexes_to_add = []
+        sum_uni_ids_with_pheno = 0
         for uni_id in current_uni_ids:
 
             df_patient_tasks = self.df_nonan[self.df_nonan["idpatients"] == uni_id]["tasks"]
@@ -304,10 +307,13 @@ class GaitGeneratorFromDFforTemporalVAE(GaitGeneratorFromDF):
                         continue
                     sampled_add_index = list(np.random.choice(add_indexes, size=1))
                     indexes_to_add += sampled_add_index
+            if list(df[(df["idpatients"] == uni_id)]["pheno_masks"])[0] == True :
+                sum_uni_ids_with_pheno += 1
 
         df_to_append = self.df_nonan.loc[indexes_to_add]
         df = pd.concat([df, df_to_append], axis=0)
-        return df
+
+        return df, sum_uni_ids_with_pheno
 
     def _construct_filtered_df(self):
         mask = (self.df_train["idpatients"].isnull() == False) & (self.df_train["task_masks"] == True)